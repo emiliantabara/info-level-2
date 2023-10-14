86601fe19181c8198dffb12e5db9001e2e8092c9
Emilian Tabără <emilian.tabara@gmail.com>
2023-10-14 15:31:54 +0300

    added tema_01

--- lesson_05/tema_01/tema_01.cpp
+++ lesson_05/tema_01/tema_01.cpp
@@ -1,20 +1,39 @@
-// tema_01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+//https://www.pbinfo.ro/probleme/1329/sir-zigzag
+
+int v[1000];
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    int dif = v[0] - v[1];
+    bool zigzag = true;
+    for (int i = 1 ; i < n-1; i++) {
+        if (dif <= 0) {
+            dif = v[i] - v[i+1];
+            if (dif <= 0) {
+                cout << "NU";
+                zigzag = false;
+                break;
+            }
+            continue;
+        }
+        else {
+            dif = v[i] - v[i+1];
+            if (dif >= 0) {
+                cout << "NU";
+                zigzag = false;
+                break;
+            }
+        }
+    }
+    if (zigzag) {
+        cout << "DA";
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
